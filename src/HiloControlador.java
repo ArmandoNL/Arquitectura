7866f33726c696b27cb3a80d3ca5132ddcdccf03
Armando Nevares <armandonl14@gmail.com>
2015-09-27 12:04:24 -0600

    mio

--- src/HiloControlador.java
+++ src/HiloControlador.java
@@ -4,7 +4,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.awt.*;
 import java.awt.event.*;
- 
+import java.util.*;
 import javax.swing.*;
 import javax.swing.filechooser.*;
 import java.util.concurrent.*;
@@ -21,7 +21,7 @@ private int quantum;
 private int latencia;
 private int tiempoBus;
 private int cantHilos=2;
-private ArrayList<Integer> vectPc;
+private Queue<Integer> vectPc;
 JFileChooser fc;
 
   
@@ -29,7 +29,6 @@ JFileChooser fc;
          
         ciclosReloj = 0;
         memTemp = new ArrayList<Integer>();
-        vectPc = new ArrayList<Integer>();
         PC = 0;
         barrier = new CyclicBarrier(cantHilos, barrierFuncion);
         fc = new JFileChooser();
@@ -41,8 +40,7 @@ JFileChooser fc;
             ciclosReloj++;       
             for(int i = 0; i < 2; i++){
             	if(!comunicadores[i].ocupado){
-                    int pcActual= vectPc.get(0);
-                    vectPc.remove(0);
+                    int pcActual= vectPc.poll();
                     if(pcActual != -1){
                     
                         comunicadores[i].write(pcActual,quantum);
@@ -63,5 +61,61 @@ JFileChooser fc;
              
         }
     };
+     
+     public void botonEjecutar(ActionEvent e) {
+  
+        //Accion de open
+        if (e.getSource() == openButton) {
+            int returnVal = fc.showOpenDialog(HiloPrincipal.this);
+  
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                 
+                //Ahora lee el archivo y guarda sus contenidos en el array de hilos
+                Charset charset = Charset.forName("US-ASCII");
+                try(BufferedReader reader = Files.newBufferedReader(file.toPath(), charset)){
+                 String instruccion = null;
+                 while((instruccion = reader.readLine()) != null){
+                    System.out.println(instruccion);
+                     
+                    String[] instrucciones = instruccion.split(" ");
+                    for(String s: instrucciones){
+                        threadarray.add(Integer.parseInt(s));
+                    }
+                 }
+                }
+                catch(IOException exc){
+                 System.err.println("IOException error");
+                }
+                
+                log.append("Opening: " + file.getName() + "." + newline);
+                cantHilos++;
+            } else {
+                log.append("Open command cancelled by user." + newline);
+            }
+            log.setCaretPosition(log.getDocument().getLength());
+  
+        //Accion de ejecutar. Llama al metodo exec()
+        } else if (e.getSource() == saveButton) {	
+            try{
+            	
+                int cHilos = Integer.parseInt(caja.getText());
+                if(cHilos == cantHilos){
+                	System.out.println("Llamando a exec");
+                    exec();
+                }
+                else{
+                    log.append("La cantidad de hilos no es igual a los hilos proporcionados\n");
+                 
+                }
+            }
+            catch(Exception t){}
+        }
+    }
+     
+     public void llenarPC(String paht)
+     {
+         
+     }
 
 }
